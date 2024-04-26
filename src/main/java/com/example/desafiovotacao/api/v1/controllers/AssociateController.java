4b549a24e8dd9b38abab727316a479eee54a8785
Gregory Bonassina <bonassina3@gmail.com>
2024-04-26 19:41:11 -0300

    FEAT: Implement structure for api version

--- src/main/java/com/example/desafiovotacao/api/v1/controllers/AssociateController.java
+++ src/main/java/com/example/desafiovotacao/api/v1/controllers/AssociateController.java
@@ -2,9 +2,7 @@ package com.example.desafiovotacao.api.v1.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.desafiovotacao.api.v1.AssociateControllerApi;
